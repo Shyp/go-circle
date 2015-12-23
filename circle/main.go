b6f92644b9d3b42380b50281431de26cfc424c0f
Kevin Burke <burke@shyp.com>
2015-12-23 11:38:40 -0800

    Support "circle open" command
    The "open" command opens the latest build in your default browser.

--- circle/main.go
+++ circle/main.go
@@ -9,6 +9,7 @@ import (
 	circle "github.com/Shyp/go-circle"
 	git "github.com/Shyp/go-git"
 	"github.com/kevinburke/bigtext"
+	"github.com/skratchdot/open-golang/open"
 )
 
 var help = `The circle binary interacts with a server that runs your tests.
@@ -19,6 +20,7 @@ Usage:
 
 The commands are:
 
+	open            Open the latest branch build in a browser.
 	update          Update to the latest version
 	version         Print the current version
 	wait            Wait for tests to finish on a branch.
@@ -58,14 +60,8 @@ func getStats(org string, project string, buildNum int) string {
 
 func doWait(flags *flag.FlagSet) {
 	args := flags.Args()
-	var branch string
-	if len(args) == 0 {
-		branchName, err := git.CurrentBranch()
-		checkError(err)
-		branch = branchName
-	} else {
-		branch = args[0]
-	}
+	branch, err := getBranchFromArgs(args)
+	checkError(err)
 
 	remote, err := git.GetRemoteURL("origin")
 	checkError(err)
@@ -137,20 +133,51 @@ func doWait(flags *flag.FlagSet) {
 	}
 }
 
+// Given a set of command line args, return the git branch or an error. Returns
+// the current git branch if no argument is specified
+func getBranchFromArgs(args []string) (string, error) {
+	if len(args) == 0 {
+		return git.CurrentBranch()
+	} else {
+		return args[0], nil
+	}
+}
+
+func doOpen(flags *flag.FlagSet) {
+	args := flags.Args()
+	branch, err := getBranchFromArgs(args)
+	checkError(err)
+	remote, err := git.GetRemoteURL("origin")
+	checkError(err)
+	cr, err := circle.GetTree(remote.Path, remote.RepoName, branch)
+	checkError(err)
+	if len(*cr) == 0 {
+		fmt.Printf("No results, are you sure there are tests for %s/%s?\n",
+			remote.Path, remote.RepoName)
+		return
+	}
+	latestBuild := (*cr)[0]
+	open.Start(latestBuild.BuildURL)
+}
+
 func main() {
 	waitflags := flag.NewFlagSet("wait", flag.ExitOnError)
+	openflags := flag.NewFlagSet("open", flag.ExitOnError)
 
 	if len(os.Args) < 2 {
 		usage()
 		return
 	}
 	switch os.Args[1] {
-	case "version":
-		fmt.Fprintf(os.Stderr, "circle version %s\n", circle.VERSION)
-		os.Exit(0)
+	case "open":
+		openflags.Parse(os.Args[2:])
+		doOpen(openflags)
 	case "update":
 		err := equinoxUpdate()
 		checkError(err)
+	case "version":
+		fmt.Fprintf(os.Stderr, "circle version %s\n", circle.VERSION)
+		os.Exit(0)
 	case "wait":
 		waitflags.Parse(os.Args[2:])
 		doWait(waitflags)
