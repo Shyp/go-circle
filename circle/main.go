68ed43d19a48788f55d76dd3d34c9131126b3428
Kevin Burke <burke@shyp.com>
2016-01-07 16:59:36 -0800

    Pull out doWait into a library
    This lets us use/call it from other binaries!

--- circle/main.go
+++ circle/main.go
@@ -4,11 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"time"
 
 	circle "github.com/Shyp/go-circle"
+	"github.com/Shyp/go-circle/wait"
 	git "github.com/Shyp/go-git"
-	"github.com/kevinburke/bigtext"
 	"github.com/skratchdot/open-golang/open"
 )
 
@@ -43,93 +42,6 @@ func checkError(err error) {
 		fmt.Fprintf(os.Stderr, err.Error())
 		fmt.Fprintf(os.Stderr, "\n")
 		os.Exit(1)
-	}
-}
-
-func roundDuration(d time.Duration, unit time.Duration) time.Duration {
-	return ((d + unit/2) / unit) * unit
-}
-
-func getStats(org string, project string, buildNum int) string {
-	build, err := circle.GetBuild(org, project, buildNum)
-	if err != nil {
-		return ""
-	}
-	return circle.BuildStatistics(build)
-}
-
-func doWait(flags *flag.FlagSet) {
-	args := flags.Args()
-	branch, err := getBranchFromArgs(args)
-	checkError(err)
-
-	remote, err := git.GetRemoteURL("origin")
-	checkError(err)
-	tip, err := git.Tip(branch)
-	checkError(err)
-	fmt.Println("Waiting for latest build on", branch, "to complete")
-	// Give CircleCI a little bit of time to start
-	time.Sleep(3 * time.Second)
-	for {
-		cr, err := circle.GetTree(remote.Path, remote.RepoName, branch)
-		checkError(err)
-		if len(*cr) == 0 {
-			fmt.Printf("No results, are you sure there are tests for %s/%s?\n",
-				remote.Path, remote.RepoName)
-			break
-		}
-		latestBuild := (*cr)[0]
-		var vcsLen int
-		var tipLen int
-		if len(latestBuild.VCSRevision) > 8 {
-			vcsLen = 8
-		} else {
-			vcsLen = len(latestBuild.VCSRevision)
-		}
-		if len(tip) > 8 {
-			tipLen = 8
-		} else {
-			tipLen = len(tip)
-		}
-		if latestBuild.VCSRevision[:tipLen] != tip {
-			fmt.Printf("Latest build in Circle is %s, waiting for %s...\n",
-				latestBuild.VCSRevision[:vcsLen], tip[:tipLen])
-			time.Sleep(5 * time.Second)
-			continue
-		}
-		var duration time.Duration
-		if latestBuild.StartTime.Valid {
-			if latestBuild.StopTime.Valid {
-				duration = latestBuild.StopTime.Time.Sub(latestBuild.StartTime.Time)
-			} else {
-				duration = time.Now().Sub(latestBuild.StartTime.Time)
-			}
-			duration = roundDuration(duration, time.Second)
-		}
-		if latestBuild.Passed() {
-			fmt.Printf("Build on %s complete!\n\n", branch)
-			fmt.Printf(getStats(remote.Path, remote.RepoName, latestBuild.BuildNum))
-			fmt.Printf("\nTests took %s. Quitting.\n", duration.String())
-			bigtext.Display(fmt.Sprintf("%s done", branch))
-			break
-		} else if latestBuild.Failed() {
-			fmt.Printf("Build failed!\n\n")
-			fmt.Printf(getStats(remote.Path, remote.RepoName, latestBuild.BuildNum))
-			fmt.Printf("\nURL: %s\n", latestBuild.BuildURL)
-			bigtext.Display("build failed")
-			os.Exit(1)
-		} else {
-			if latestBuild.Status == "running" {
-				fmt.Printf("Running (%s elapsed)\n", duration.String())
-			} else {
-				fmt.Printf("Status is %s, trying again\n", latestBuild.Status)
-			}
-			if float32(duration) < (2.5 * float32(time.Minute)) {
-				time.Sleep(10 * time.Second)
-			} else {
-				time.Sleep(5 * time.Second)
-			}
-		}
 	}
 }
 
@@ -180,7 +92,11 @@ func main() {
 		os.Exit(0)
 	case "wait":
 		waitflags.Parse(os.Args[2:])
-		doWait(waitflags)
+		args := waitflags.Args()
+		branch, err := getBranchFromArgs(args)
+		checkError(err)
+		err = wait.Wait(branch)
+		checkError(err)
 	default:
 		usage()
 	}
---
+++ wait/wait.go
@@ -0,0 +1,100 @@
+package wait
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Shyp/go-circle"
+	"github.com/Shyp/go-git"
+	"github.com/kevinburke/bigtext"
+)
+
+func roundDuration(d time.Duration, unit time.Duration) time.Duration {
+	return ((d + unit/2) / unit) * unit
+}
+
+func getStats(org string, project string, buildNum int) string {
+	build, err := circle.GetBuild(org, project, buildNum)
+	if err != nil {
+		return ""
+	}
+	return circle.BuildStatistics(build)
+}
+
+func Wait(branch string) error {
+	remote, err := git.GetRemoteURL("origin")
+	if err != nil {
+		return err
+	}
+	tip, err := git.Tip(branch)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Waiting for latest build on", branch, "to complete")
+	// Give CircleCI a little bit of time to start
+	time.Sleep(3 * time.Second)
+	for {
+		cr, err := circle.GetTree(remote.Path, remote.RepoName, branch)
+		if err != nil {
+			return err
+		}
+		if len(*cr) == 0 {
+			return fmt.Errorf("No results, are you sure there are tests for %s/%s?\n",
+				remote.Path, remote.RepoName)
+			break
+		}
+		latestBuild := (*cr)[0]
+		var vcsLen int
+		var tipLen int
+		if len(latestBuild.VCSRevision) > 8 {
+			vcsLen = 8
+		} else {
+			vcsLen = len(latestBuild.VCSRevision)
+		}
+		if len(tip) > 8 {
+			tipLen = 8
+		} else {
+			tipLen = len(tip)
+		}
+		if latestBuild.VCSRevision[:tipLen] != tip {
+			fmt.Printf("Latest build in Circle is %s, waiting for %s...\n",
+				latestBuild.VCSRevision[:vcsLen], tip[:tipLen])
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		var duration time.Duration
+		if latestBuild.StartTime.Valid {
+			if latestBuild.StopTime.Valid {
+				duration = latestBuild.StopTime.Time.Sub(latestBuild.StartTime.Time)
+			} else {
+				duration = time.Now().Sub(latestBuild.StartTime.Time)
+			}
+			duration = roundDuration(duration, time.Second)
+		}
+		if latestBuild.Passed() {
+			fmt.Printf("Build on %s complete!\n\n", branch)
+			fmt.Printf(getStats(remote.Path, remote.RepoName, latestBuild.BuildNum))
+			fmt.Printf("\nTests on %s took %s. Quitting.\n", branch, duration.String())
+			bigtext.Display(fmt.Sprintf("%s done", branch))
+			break
+		} else if latestBuild.Failed() {
+			fmt.Printf(getStats(remote.Path, remote.RepoName, latestBuild.BuildNum))
+			fmt.Printf("\nURL: %s\n", latestBuild.BuildURL)
+			err = fmt.Errorf("Build %s failed!\n\n", branch)
+			bigtext.Display("build failed")
+			return err
+		} else {
+			if latestBuild.Status == "running" {
+				fmt.Printf("Running (%s elapsed)\n", duration.String())
+			} else {
+				fmt.Printf("Status is %s, trying again\n", latestBuild.Status)
+			}
+			if float32(duration) < (2.5 * float32(time.Minute)) {
+				time.Sleep(10 * time.Second)
+			} else {
+				time.Sleep(5 * time.Second)
+			}
+		}
+	}
+	return nil
+}
