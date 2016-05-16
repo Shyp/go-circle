e51e36211e4aad0eee53b781e2c5d6a3da64bab0
Kevin Burke <burke@shyp.com>
2016-05-16 15:39:05 -0700

    Add additional command to download artifacts
    Run "circle download-artifacts <build-num>" to download all artifacts for
    a given build.

--- circle.go
+++ circle.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path"
+	"path/filepath"
 	"time"
 
 	"github.com/Shyp/go-types"
@@ -23,18 +25,18 @@ const VERSION = "0.22"
 const baseUri = "https://circleci.com/api/v1/project"
 
 type TreeBuild struct {
-	BuildNum	int	`json:"build_num"`
-	BuildURL	string	`json:"build_url"`
-	CompareURL	string	`json:"compare"`
+	BuildNum   int    `json:"build_num"`
+	BuildURL   string `json:"build_url"`
+	CompareURL string `json:"compare"`
 	// Tree builds have a `previous_successful_build` field but as far as I can
 	// tell it is always null. Instead this field is set
-	Previous	PreviousBuild	`json:"previous"`
-	QueuedAt	types.NullTime	`json:"queued_at"`
-	Status		string		`json:"status"`
-	StartTime	types.NullTime	`json:"start_time"`
-	StopTime	types.NullTime	`json:"stop_time"`
-	UsageQueuedAt	types.NullTime	`json:"usage_queued_at"`
-	VCSRevision	string		`json:"vcs_revision"`
+	Previous      PreviousBuild  `json:"previous"`
+	QueuedAt      types.NullTime `json:"queued_at"`
+	Status        string         `json:"status"`
+	StartTime     types.NullTime `json:"start_time"`
+	StopTime      types.NullTime `json:"stop_time"`
+	UsageQueuedAt types.NullTime `json:"usage_queued_at"`
+	VCSRevision   string         `json:"vcs_revision"`
 }
 
 func (tb TreeBuild) Passed() bool {
@@ -53,33 +55,40 @@ func (tb TreeBuild) Failed() bool {
 	return tb.Status == "failed" || tb.Status == "timedout" || tb.Status == "no_tests" || tb.Status == "infrastructure_fail"
 }
 
+type CircleArtifact struct {
+	Path       string `json:"path"`
+	PrettyPath string `json:"pretty_path"`
+	NodeIndex  uint8  `json:"node_index"`
+	Url        string `json:"url"`
+}
+
 type CircleBuild struct {
-	Parallel		uint8		`json:"parallel"`
-	PreviousSuccessfulBuild	PreviousBuild	`json:"previous_successful_build"`
-	QueuedAt		types.NullTime	`json:"queued_at"`
-	Steps			[]Step		`json:"steps"`
-	UsageQueuedAt		types.NullTime	`json:"usage_queued_at"`
+	Parallel                uint8          `json:"parallel"`
+	PreviousSuccessfulBuild PreviousBuild  `json:"previous_successful_build"`
+	QueuedAt                types.NullTime `json:"queued_at"`
+	Steps                   []Step         `json:"steps"`
+	UsageQueuedAt           types.NullTime `json:"usage_queued_at"`
 }
 
 type PreviousBuild struct {
-	BuildNum	int	`json:"build_num"`
+	BuildNum int `json:"build_num"`
 	// would be neat to make this a time.Duration, easier to use the passed in
 	// value.
-	Status	string	`json:"status"`
+	Status string `json:"status"`
 
-	BuildDurationMs	int	`json:"build_time_millis"`
+	BuildDurationMs int `json:"build_time_millis"`
 }
 
 type Step struct {
-	Name	string		`json:"name"`
-	Actions	[]Action	`json:"actions"`
+	Name    string   `json:"name"`
+	Actions []Action `json:"actions"`
 }
 
 type Action struct {
-	Name		string		`json:"name"`
-	OutputURL	URL		`json:"output_url"`
-	Runtime		CircleDuration	`json:"run_time_millis"`
-	Status		string		`json:"status"`
+	Name      string         `json:"name"`
+	OutputURL URL            `json:"output_url"`
+	Runtime   CircleDuration `json:"run_time_millis"`
+	Status    string         `json:"status"`
 }
 
 func (a *Action) Failed() bool {
@@ -96,6 +105,10 @@ func getBuildUri(org string, project string, build int, token string) string {
 
 func getCancelUri(org string, project string, build int, token string) string {
 	return fmt.Sprintf("%s/%s/%s/%d/cancel?circle-token=%s", baseUri, org, project, build, token)
+}
+
+func getArtifactsUri(org string, project string, build int, token string) string {
+	return fmt.Sprintf("%s/%s/%s/%d/artifacts?circle-token=%s", baseUri, org, project, build, token)
 }
 
 type CircleTreeResponse []TreeBuild
@@ -158,6 +171,55 @@ func GetBuild(org string, project string, buildNum int) (*CircleBuild, error) {
 	var cb CircleBuild
 	err = d.Decode(&cb)
 	return &cb, err
+}
+
+func GetArtifactsForBuild(org string, project string, buildNum int) ([]*CircleArtifact, error) {
+	token, err := getToken(org)
+	if err != nil {
+		return []*CircleArtifact{}, err
+	}
+	uri := getArtifactsUri(org, project, buildNum, token)
+	// TODO handle build not found, you get back {"message": "Build not found"}
+	body, err := makeRequest("GET", uri)
+	if err != nil {
+		return []*CircleArtifact{}, err
+	}
+	defer body.Close()
+	var r io.Reader
+	if os.Getenv("CIRCLE_DEBUG") == "true" {
+		r = io.TeeReader(body, os.Stdout)
+	} else {
+		r = body
+	}
+	var arts []*CircleArtifact
+	if err = json.NewDecoder(r).Decode(&arts); err != nil {
+		return arts, err
+	}
+	return arts, nil
+}
+
+func DownloadArtifact(artifact *CircleArtifact, directory string, org string) error {
+	token, err := getToken(org)
+	if err != nil {
+		return err
+	}
+	fname := fmt.Sprintf("%d.%s", artifact.NodeIndex, path.Base(artifact.Url))
+	fmt.Fprintf(os.Stderr, "Downloading artifact to %s\n", fname)
+	f, err := os.Create(filepath.Join(directory, fname))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	url := fmt.Sprintf("%s?circle-token=%s", artifact.Url, token)
+	body, err := makeRequest("GET", url)
+	if err != nil {
+		return err
+	}
+	defer body.Close()
+	if _, err := io.Copy(f, body); err != nil {
+		return err
+	}
+	return nil
 }
 
 func CancelBuild(org string, project string, buildNum int) (*CircleBuild, error) {
--- circle/main.go
+++ circle/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"strconv"
+	"sync"
 
 	circle "github.com/Shyp/go-circle"
 	"github.com/Shyp/go-circle/wait"
@@ -11,7 +15,7 @@ import (
 	"github.com/skratchdot/open-golang/open"
 )
 
-var help = `The circle binary interacts with a server that runs your tests.
+const help = `The circle binary interacts with a server that runs your tests.
 
 Usage: 
 
@@ -19,13 +23,16 @@ Usage:
 
 The commands are:
 
-	open            Open the latest branch build in a browser.
-	update          Update to the latest version
-	version         Print the current version
-	wait            Wait for tests to finish on a branch.
+	open                Open the latest branch build in a browser.
+	update              Update to the latest version
+	version             Print the current version
+	wait                Wait for tests to finish on a branch.
+	download-artifacts  Download all artifacts.
 
 Use "circle help [command]" for more information about a command.
 `
+
+const downloadUsage = `usage: download-artifacts <build-num>`
 
 func usage() {
 	fmt.Fprintf(os.Stderr, help)
@@ -39,9 +46,7 @@ func init() {
 
 func checkError(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, err.Error())
-		fmt.Fprintf(os.Stderr, "\n")
-		os.Exit(1)
+		log.Fatal(err)
 	}
 }
 
@@ -72,15 +77,56 @@ func doOpen(flags *flag.FlagSet) {
 	open.Start(latestBuild.BuildURL)
 }
 
+func doDownload(flags *flag.FlagSet) error {
+	buildStr := flags.Arg(0)
+	val, err := strconv.Atoi(buildStr)
+	if err != nil {
+		return err
+	}
+	remote, err := git.GetRemoteURL("origin")
+	if err != nil {
+		return err
+	}
+	arts, err := circle.GetArtifactsForBuild(remote.Path, remote.RepoName, val)
+	if err != nil {
+		return err
+	}
+	var wg sync.WaitGroup
+	tempDir, err := ioutil.TempDir("", "circle-artifacts")
+	if err != nil {
+		return err
+	}
+	for _, art := range arts {
+		wg.Add(1)
+		go func(a *circle.CircleArtifact) {
+			if err := circle.DownloadArtifact(a, tempDir, remote.Path); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+			wg.Done()
+		}(art)
+	}
+	wg.Wait()
+	fmt.Fprintf(os.Stderr, "Wrote all artifacts for build %d to %s\n", val, tempDir)
+	return nil
+}
+
 func main() {
 	waitflags := flag.NewFlagSet("wait", flag.ExitOnError)
 	openflags := flag.NewFlagSet("open", flag.ExitOnError)
+	downloadflags := flag.NewFlagSet("download-artifacts", flag.ExitOnError)
+	downloadflags.Usage = func() {
+		fmt.Fprintf(os.Stderr, "%s\n\n", downloadUsage)
+		downloadflags.PrintDefaults()
+		os.Exit(1)
+	}
 
-	if len(os.Args) < 2 {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
 		usage()
 		return
 	}
-	switch os.Args[1] {
+	switch flag.Arg(0) {
 	case "open":
 		openflags.Parse(os.Args[2:])
 		doOpen(openflags)
@@ -89,7 +135,7 @@ func main() {
 		checkError(err)
 	case "version":
 		fmt.Fprintf(os.Stderr, "circle version %s\n", circle.VERSION)
-		os.Exit(0)
+		os.Exit(1)
 	case "wait":
 		waitflags.Parse(os.Args[2:])
 		args := waitflags.Args()
@@ -97,6 +143,16 @@ func main() {
 		checkError(err)
 		err = wait.Wait(branch)
 		checkError(err)
+	case "download-artifacts":
+		if len(args) == 1 {
+			fmt.Fprintf(os.Stderr, "usage: download-artifacts <build-number>\n")
+			os.Exit(1)
+		}
+		downloadflags.Parse(args[1:])
+		if err := doDownload(downloadflags); err != nil {
+			log.Fatal(err)
+		}
+
 	default:
 		usage()
 	}
