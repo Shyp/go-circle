785f8dc614e7ca989e3aeeb14bb92da364917892
Kevin Burke <kev@inburke.com>
2017-02-23 10:58:46 -0800

    Add help text and fix various lint errors
    - Run a linter when the tests run that checks for various formatting problems
    
    - Fix problems suggested by the linter including:
    
        - Don't call os.Exit from the flag.Usage - flag has an ErrorHandling
          configuration that you should use to configure behavior instead, and it
          does the right thing by default (ExitOnError)
    
        - use fmt.Print instead of fmt.Printf when the first argument is dynamic
    
        - fix some other print statements
    
        - use os.Stderr.WriteString instead of log.Fatal to kill a process
    
        - fix tabbing in builds/build.go

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 .PHONY: install test
 
+STATICCHECK := $(shell command -v staticcheck)
+BUMP_VERSION := $(shell command -v bump_version)
+
 install:
 	go install ./...
 
@@ -7,11 +10,21 @@ build:
 	go get ./...
 	go build ./...
 
-test: install
+lint: 
+	go vet ./...
+ifndef STATICCHECK
+	go get -u honnef.co/go/tools/cmd/staticcheck
+endif
+	staticcheck ./...
+
+test: install lint
 	go test -v -race ./...
 
 release:
 	git checkout master
+ifndef BUMP_VERSION
+	go get github.com/Shyp/bump_version
+endif
 	bump_version minor circle.go
 	git push origin master
 	git push origin master --tags
--- builds/build.go
+++ builds/build.go
@@ -1,80 +1,80 @@
 package build
 
 import (
-  "fmt"
+	"fmt"
 
-  "github.com/Shyp/go-circle"
-  "github.com/Shyp/go-git"
+	"github.com/Shyp/go-circle"
+	"github.com/Shyp/go-git"
 )
 
 func inSlice(a string, list []string) bool {
-  for _, b := range list {
-    if b == a {
-        return true
-    }
-  }
-  return false
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
 // GetBuilds gets the status of the 5 most recent Circle builds for a branch
 func GetBuilds(branch string) error {
-  // This throws if the branch doesn't exist
-  if _, err := git.Tip(branch); err != nil {
-    return err
-  }
+	// This throws if the branch doesn't exist
+	if _, err := git.Tip(branch); err != nil {
+		return err
+	}
 
-  fmt.Println("\nFetching recent builds for", branch, "starting with most recent commit\n")
+	fmt.Printf("\nFetching recent builds for %s starting with most recent commit\n\n", branch)
 
-  remote, err := git.GetRemoteURL("origin")
-  if err != nil {
-    return err
-  }
+	remote, err := git.GetRemoteURL("origin")
+	if err != nil {
+		return err
+	}
 
-  cr, err := circle.GetTree(remote.Path, remote.RepoName, branch)
-  if err != nil {
-    return err
-  }
+	cr, err := circle.GetTree(remote.Path, remote.RepoName, branch)
+	if err != nil {
+		return err
+	}
 
-  buildCount := 0
+	buildCount := 0
 
-  if len(*cr) < 5 {
-    buildCount = len(*cr)
-  } else {
-    buildCount = 5 // Limited to 5 most recent builds.
-  }
+	if len(*cr) < 5 {
+		buildCount = len(*cr)
+	} else {
+		buildCount = 5 // Limited to 5 most recent builds.
+	}
 
-  for i := 0; i < buildCount; i++ {
-    build := (*cr)[i]
-    ghUrl, url, status := build.CompareURL, build.BuildURL, build.Status
+	for i := 0; i < buildCount; i++ {
+		build := (*cr)[i]
+		ghUrl, url, status := build.CompareURL, build.BuildURL, build.Status
 
-    // Based on the status of the build, change the color of status print out
-    if build.Passed() {
-      status = fmt.Sprintf("\033[38;05;119m%-8s\033[0m", status)
-    } else if build.NotRunning(){
-      status = fmt.Sprintf("\033[38;05;20m%-8s\033[0m", status)
-    } else if build.Failed() {
-      status = fmt.Sprintf("\033[38;05;160m%-8s\033[0m", status)
-    } else if build.Running() {
-      status = fmt.Sprintf("\033[38;05;80m%-8s\033[0m", status)
-    } else {
-      status = fmt.Sprintf("\033[38;05;0m%-8s\033[0m", status)
-    }
+		// Based on the status of the build, change the color of status print out
+		if build.Passed() {
+			status = fmt.Sprintf("\033[38;05;119m%-8s\033[0m", status)
+		} else if build.NotRunning() {
+			status = fmt.Sprintf("\033[38;05;20m%-8s\033[0m", status)
+		} else if build.Failed() {
+			status = fmt.Sprintf("\033[38;05;160m%-8s\033[0m", status)
+		} else if build.Running() {
+			status = fmt.Sprintf("\033[38;05;80m%-8s\033[0m", status)
+		} else {
+			status = fmt.Sprintf("\033[38;05;0m%-8s\033[0m", status)
+		}
 
-    fmt.Println(url, status, ghUrl)
+		fmt.Println(url, status, ghUrl)
 
-  }
+	}
 
-  fmt.Println("\nMost recent build statuses fetched!")
+	fmt.Println("\nMost recent build statuses fetched!")
 
-  return nil
+	return nil
 }
 
 // CancelBuild cancels a build (as specified by the build number)
 func CancelBuild(org string, project string, buildNum int) string {
-  fmt.Printf("\nCanceling build: %d for %s\n\n", buildNum, project)
-  if _, err := circle.CancelBuild(org, project, buildNum); err != nil {
-    return ""
-  }
+	fmt.Printf("\nCanceling build: %d for %s\n\n", buildNum, project)
+	if _, err := circle.CancelBuild(org, project, buildNum); err != nil {
+		return ""
+	}
 
-  return ""
+	return ""
 }
